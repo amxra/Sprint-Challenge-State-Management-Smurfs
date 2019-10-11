5a11871bcdc8f07d5aa8166998ee12fe52654761
amxra <amiradediran@gmail.com>
2019-10-11 20:56:46 +0100

    Inject monster reducer into store

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -11,14 +11,22 @@ import smurfReducer from '../state/reducers';
 const monsterReducer = combineReducers({
   smurf: smurfReducer.smurfReducer,
 })
+
+
+const store = createStore(
+  monsterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+
+
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        
-
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
